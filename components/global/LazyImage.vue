5bfeeee78189d6cdc46a0eed071b9b264651ab75
Jonathan <machadofjonathan@gmail.com>
2020-08-03 19:06:12 +0100

    chore: LazyImage.vue implemented

---
+++ components/global/LazyImage.vue
@@ -0,0 +1,14 @@
+<template>
+  <img :src="require(`~/assets/images/${src}`)" alt="" />
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
